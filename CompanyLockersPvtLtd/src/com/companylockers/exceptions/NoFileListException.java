37c2030362277173dcee5e7d0e06da861403f4cc
Anupkumr Seth <anupseth11@yahoo.com>
2021-08-08 13:22:01 +0530

    Custom exception to handle exceptons for getting files in sorted order functionality

---
+++ CompanyLockersPvtLtd/src/com/companylockers/exceptions/NoFileListException.java
@@ -0,0 +1,14 @@
+package com.companylockers.exceptions;
+
+public class NoFileListException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoFileListException(String message) {
+		super(message);
+	}
+
+}
