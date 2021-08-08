0d39ff5f6a6e4b9a0ca1fac899cc2b130f308c1a
Anupkumr Seth <anupseth11@yahoo.com>
2021-08-08 14:45:21 +0530

    Adding new custom exception class to handle exxeptions while trying to add files to the root directory

---
+++ CompanyLockersPvtLtd/src/com/companylockers/exceptions/FileCreationException.java
@@ -0,0 +1,14 @@
+package com.companylockers.exceptions;
+
+public class FileCreationException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FileCreationException(String message) {
+		super(message);
+	}
+
+}
