784e8c83aceefc78590e928f5d1fa837566b9d9d
Anupkumr Seth <anupseth11@yahoo.com>
2021-08-09 23:37:28 +0530

    Added java doc to exception classes

--- CompanyLockersPvtLtd/src/com/companylockers/exceptions/FileCreationException.java
+++ CompanyLockersPvtLtd/src/com/companylockers/exceptions/FileCreationException.java
@@ -1,5 +1,12 @@
 package com.companylockers.exceptions;
 
+/**
+ * This custom exception class will be used to give user more application specific exception
+ * @author Anupkumar Seth
+ * @version 1.0
+ * @since 2021-08-08
+ *
+ */
 public class FileCreationException extends RuntimeException {
 	
 	/**
